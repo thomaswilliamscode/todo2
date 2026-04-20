7d2bb606b50d42f49e57aba2838071f4c359a498
Thomas Williams <thomas.williams.code@gmail.com>
2026-04-20 13:59:51 -0400

    trying to create folder data

--- src/components/add-folder-or-list/add-folder-or-list.jsx
+++ src/components/add-folder-or-list/add-folder-or-list.jsx
@@ -1,7 +1,47 @@
+import { pushData, maxPosition } from '../db-logic/db-logic'
+import {useState} from 'react'
+
 export default function Add () {
+    const [ input, setInput ] = useState('')
+    const [ type, setType ] = useState('folders')
+    const [ pos, setPos ] = useState(0)
+
+    const inputChange = (data) => {
+        setInput(data)
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        let newPos = await maxPosition(type)
+        console.log(newPos)
+
+        pushData(input, type)
+
+        setInput('')
+
+    }
+    const optionChange = (value) => {
+        setType(value)
+    }
     return (
         <>
-            <div>Add</div>
+            <form onSubmit={handleSubmit}>
+                < input 
+                type='text' 
+                name='name' 
+                value={input}
+                onChange={(e)=> inputChange(e.target.value)}
+                />
+                <select id='type'
+                    value={type}
+                    onChange = { (e) => optionChange(e.target.value)}>
+                    <option value='folders'>Folder</option>
+                    <option value='lists'>List</option>
+                    <option value='todos'>Todo</option>
+                </select>
+                
+                < input type='submit' value='Submit'/>
+            </form>
         </>
     )
 }
