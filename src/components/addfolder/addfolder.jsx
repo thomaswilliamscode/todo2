35bfbb09bee581e58023f0a1469605c9cf336059
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2026-05-05 11:46:35 -0400

    Updated addfolder.jsx

--- src/components/addfolder/addfolder.jsx
+++ src/components/addfolder/addfolder.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import { pushData } from '../db-logic/db-logic'
 
 export default function AddFolder() {
   const [ folderName, setFolderName ] = useState('')
   function formSubmit (e) {
     e.preventDefault()
     // send folder name to database
-
+    pushData(fodlerName)
     // reset folderName to empty 
     setFolderName('')
   }
