8d1bd952051e7f2b819aac6fdd455be4f4803ba8
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2026-05-05 15:54:40 -0400

    Updated fodlerProvided.js

--- src/components/context/fodlerProvided.js
+++ src/components/context/fodlerProvided.js
@@ -1,3 +1,15 @@
 import { createContext, useContext, useState } from "react"
 
 const AppContext = createContext()
+
+export function folderProvider({ children }) {
+  const [folders, setFolders] = useState([])
+
+  return (
+    <AppContext.Provider value={{ folders, setFolders }}>
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export { AppContext }
