19cdb3c1d1b0f8569da7602ba44fa1f0774fd0ad
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2026-05-05 11:15:03 -0400

    Updated addfolder.jsx

--- src/components/addfolder/addfolder.jsx
+++ src/components/addfolder/addfolder.jsx
@@ -1,6 +1,10 @@
-import { formSubmit } from './helpers'
+
 
 export default function AddFolder() {
+  function formSubmit (e) {
+    e.preventDefault()
+    console.log('we loggin bruh')
+  }
     return (
         <div>
             <form onSubmit={formSubmit}>
