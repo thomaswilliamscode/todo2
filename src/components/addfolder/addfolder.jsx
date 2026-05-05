342e76ac1cda6b330068d37b73ea7c877f623503
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2026-05-05 10:07:06 -0400

    Updated addfolder.jsx

--- src/components/addfolder/addfolder.jsx
+++ src/components/addfolder/addfolder.jsx
@@ -1,18 +1,6 @@
 import { formSubmit } from './helpers'
-import { useState } from 'react'
 
 export default function AddFolder() {
-  const [ folderName, setFolderName ] = useState('')
-
-  function formSubmit(e) {
-    e.preventDefault()
-    // folder name needs to be submitted to database
-
-    // setFolderName back to empty string
-    
-  }
-
-  
     return (
         <div>
             <form onSubmit={formSubmit}>
