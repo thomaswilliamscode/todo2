665e6107e210dc350f441ee1889b04246bd6f3e3
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2026-05-08 17:10:22 +0000

    can properly add lists, and it properly rerenders

--- src/components/addlist/addlist.jsx
+++ src/components/addlist/addlist.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import { pushData, maxPosition, getData } from '../db-logic/db-logic'
 import { ListContext } from '../../context/listContext'
 import { FolderContext } from '../../context/folderContext'
@@ -9,12 +9,26 @@ export default function AddList() {
   const { getLists, setGetLists } = useContext(ListContext)
   const { folders, setFolders } = useContext(FolderContext)
   const [ listName, setListName ] = useState('')
+  const [ activeFolder, setActiveFolder ] = useState('')
+
+  useEffect ( () => {
+    const value = localStorage.getItem('folders')
+    if(!activeFolder && value) { 
+      const parsed = JSON.parse(value)
+      const found = parsed.find( (folderObj) => {
+        return folderObj.position === 0
+      })
+      const {id} = found
+      setActiveFolder(id)
+    }
+  }, [])
 
   async function formSubmit (e) {
     e.preventDefault()
     // // send list name to database
     const newData ={
       name:listName,
+      folder_id: activeFolder
     }
     setListName('')
     let posObj = await maxPosition(table)
@@ -35,11 +49,11 @@ export default function AddList() {
                   onChange={(e) => setListName((e.target.value))}
                   placeholder='List Name'/>
                 <input type='submit' value='Submit'/>
-                <select onChange={(e)=> console.log('test: ', )}>
-                  {folders && folders.map( (folder) => {
+                <select onChange={(e)=> setActiveFolder(e.target.value)}>
+                  {folders && folders.map( (folder, index) => {
                     const { name, id} = folder;
                     return (
-                      <option key={id} value={folder}>{name}</option>
+                      <option key={id} value={id}>{name}</option>
                     )  
                   })}
                   
