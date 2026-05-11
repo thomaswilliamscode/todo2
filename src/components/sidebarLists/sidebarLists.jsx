b5f5df8bcfd7b0e5dfefb7fcce4a9cb6d26bffb2
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2026-05-11 20:47:27 +0000

    struggled for 45 mins trying to destructure an obj when i had multiple objects inside of an array.....

--- src/components/sidebarLists/sidebarLists.jsx
+++ src/components/sidebarLists/sidebarLists.jsx
@@ -5,16 +5,27 @@ import { NavLink } from "react-router-dom";
 import {ListContext} from '../../context/listContext'
 
 export default function SidebarLists (props) {
+    const {listIds, setListIds} = useContext(ListContext)
     const [ lists, setLists ] = useState([])
     const {id, name, position} = props.info
     const table = 'lists'
     useEffect( () => {
         async function fetchData() {
             const lists = await getData(table, id)
+            lists.map ( (list) => {
+                const {id: list_id, } = list
+                console.log(list)
+            }) 
             setLists(lists)
+
+            
         }
         fetchData()
+
+        
+        
     }, [id])
+    console.log(listIds)
     return (
         <ul id='list-container'>
             {lists && lists.map( (list) => {
--- src/context/listContext.jsx
+++ src/context/listContext.jsx
@@ -4,9 +4,9 @@ export const ListContext = createContext()
 
 export function ListProvider ({children}) {
     const [ getLists, setGetLists ] = useState('got')
-    const { lists, setLists } = useState({})
+    const [ listIds, setListIds ] = useState({})
     return (
-        <ListContext.Provider value={{lists, setLists}}>
+        <ListContext.Provider value={{listIds, setListIds}}>
             {children}
         </ListContext.Provider>
     )
